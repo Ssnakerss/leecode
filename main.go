7d9e06f966a5cf1c0c61ba1af0c612fcca8f8804
i.krivokon <i.krivokon@samsung.com>
2025-07-28 16:42:18 +0300

    76. Minimum Window Substring in progress
    use map and goroutines

--- main.go
+++ main.go
@@ -2,31 +2,68 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/Ssnakerss/leecode/funcs"
+	"maps"
 )
 
 func main() {
-	n3 := &funcs.ListNode{Val: 5, Next: nil}
-	n2 := &funcs.ListNode{Val: 4, Next: n3}
-	n1 := &funcs.ListNode{Val: 1, Next: n2}
+	s := "ADOBECODEBANC"
+	t := "ABC"
 
-	n33 := &funcs.ListNode{Val: 4, Next: nil}
-	n22 := &funcs.ListNode{Val: 3, Next: n33}
-	n11 := &funcs.ListNode{Val: 1, Next: n22}
+	tKey := makeKeysMap(t)
 
-	n222 := &funcs.ListNode{Val: 6, Next: nil}
-	n111 := &funcs.ListNode{Val: 2, Next: n222}
-
-	arr := []*funcs.ListNode{n1, n11, n111}
-
-	fmt.Println(n1.String())
-	fmt.Println(n11.String())
-	fmt.Println(n111.String())
-
-	res := funcs.MergeKLists2(arr)
-	if res != nil {
-		fmt.Println(res.String())
+	for i, r := range s {
+		if i >= len(s)-len(t) {
+			break
+		}
+		if _, ok := tKey[string(r)]; ok {
+			tKeyCopy := map[string]int{}
+			maps.Copy(tKeyCopy, tKey)
+			w, l := findWindow(s[i:], tKeyCopy)
+			fmt.Printf("%s: %d\n", w, l)
+		}
 	}
-
 }
+
+func makeKeysMap(t string) map[string]int {
+	if len(t) == 0 {
+		return nil
+	}
+	tKey := map[string]int{}
+	for _, r := range t {
+		if _, ok := tKey[string(r)]; !ok {
+			tKey[string(r)] = 1
+		} else {
+			tKey[string(r)]++
+		}
+	}
+	return tKey
+}
+func findWindow(s string, keys map[string]int) (string, int) {
+	start := -1
+	end := -1
+	for i, r := range s {
+		//match kekys list
+		if v, ok := keys[string(r)]; ok {
+			if v > 0 {
+				keys[string(r)]--
+				v--
+				if v == 0 {
+					delete(keys, string(r))
+				}
+				if start == -1 {
+					start = i
+				}
+				end = i
+			}
+		}
+	}
+	if start == -1 || end == -1 || len(keys) > 0 {
+		return "", 0
+	}
+	return s[start : end+1], end - start + 1
+}
+
+// // Minimum window substring
+// func minWindow(s string, t string) string {
+// 	return ""
+// }
